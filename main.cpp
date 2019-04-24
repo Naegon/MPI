13e6f005f121b99e960300a11d21847ed451591e
Guillaume Hamel <guillaume.hamel@efrei.net>
2019-04-24 12:34:54 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, Swap-Team!" << std::endl;
+    std::cout << "Hello, ";
+    std::cout << "Swap-Team!" << std::endl;
     int a;
     return 0;
 }
